b605e86c719ef4e141745a17cd8e3b82add3fe15
Gregory Daynes <email@gregdaynes.com>
2024-07-19 17:57:03 -0700

    chore: bootstrap project with fastify-cli

---
+++ app.js
@@ -0,0 +1,26 @@
+import path from 'path'
+import AutoLoad from '@fastify/autoload'
+
+// Pass --options via CLI arguments in command to enable these options.
+export const options = {}
+
+export default async function (fastify, opts) {
+  // Place here your custom code!
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(import.meta.dirname, 'plugins'),
+    options: Object.assign({}, opts),
+  })
+
+  // This loads all plugins defined in routes
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(import.meta.dirname, 'routes'),
+    options: Object.assign({}, opts),
+  })
+}
